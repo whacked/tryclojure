eade838c37748e6c257d05d25c4d046814be4f6e
natto <natto@natto-tpy.none>
2015-08-16 21:37:28 -0400

    working overtone import + eval hack

--- README.md
+++ README.md
@@ -1,8 +1,24 @@
-# tryclojure
+# tryclojure with overtone HACK
 
 TryClojure is a online Clojure REPL written using Noir and Chris Done's jquery console (you're awesome, Chris).
 
 [<img src="https://secure.travis-ci.org/Raynes/tryclojure.png"/>](http://travis-ci.org/Raynes/tryclojure)
+
+## NOT SAFE FOR PUBLIC USE!!!
+
+this is modified to load the [overtone](https://github.com/overtone/overtone)
+library so that you can interact with overtone through the tryclj. It is
+intended to be used in demonstration settings.
+
+see `views/eval.clj` for the hack. All we do, is *bypass the sandbox*, and
+**eval-ing expressions as-is**! You probably do not want to be using this!
+
+### Other caveats
+
+- forcing all incoming expressions to evaluate in the `tryclojure.views.eval` ns
+- large requests will cause `WARN:oejh.HttpParser:HttpParser Full` issue.
+  the expression is not eval-ed, and the page needs to be reloaded.
+  workaround is simply to use a smaller expression.
 
 ## Usage
 
--- src/tryclojure/views/eval.clj
+++ src/tryclojure/views/eval.clj
@@ -1,15 +1,43 @@
 (ns tryclojure.views.eval
+  (:use [overtone.live]
+        [overtone.inst.piano]
+        [overtone.inst.sampled-piano]
+        [overtone.synth.stringed])
   (:require [tryclojure.models.eval :refer [eval-request]]
-            [noir.response :as resp]))
+            [noir.response :as resp]
+            [overtone.inst.synth :as synth]
+            [clojure.pprint :as pp])
+  (:import java.io.StringWriter))
 
-(defn eval-json [expr jsonp]
-  (let [{:keys [expr result error message] :as res} (eval-request expr)
-        data (if error
-               res
-               (let [[out res] result]
-                 {:expr (pr-str expr)
-                  :result (str out (pr-str res))}))]
-    
-    (if jsonp
-      (resp/jsonp jsonp data)
-      (resp/json data))))
+(defn eval-json [expr-orig jsonp]
+  ;; initially was explicitly wrapping expr-orig with the imports we want, and
+  ;; sending it to the sandbox; but then we have to deal with the thread
+  ;; timeout control. Will do another day...
+  ;; FYI, looks like this:
+  ;; (str
+  ;;  "(do
+  ;;    (require '[overtone.inst.synth :as synth])
+  ;;    (use '[overtone.live])
+  ;;    (use '[overtone.inst.piano])
+  ;;    (use '[overtone.inst.sampled-piano])
+  ;;    (use '[overtone.synth.stringed])"
+  ;;  expr-orig
+  ;;  ")")
+  (let [expr expr-orig
+        form (read-string expr)
+        ]
+    (pp/pprint form)
+    (let [{:keys [expr result error message] :as res} ;; (eval-request expr)
+          {:expr form
+           :result (let [res (binding [*ns* (find-ns 'tryclojure.views.eval)]
+                               (eval form))
+                         out (StringWriter.)]
+                     [out res])}
+          data (if error
+                 res
+                 (let [[out res] result]
+                   {:expr (pr-str expr)
+                    :result (str out (pr-str res))}))]
+      (if jsonp
+        (resp/jsonp jsonp data)
+        (resp/json data)))))
